a8702b1b65c6ad6e8c9a0c0834ddac617546eeb2
Jake Lira <llira09@gmail.com>
2021-02-17 15:07:58 -0600

    added route, contoller and db connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,32 @@
-const express = require('express')
+import express from 'express'
+import router from './routes/auth'
+// import { readdirSync } from 'fs'
+import cors from 'cors'
+import mongoose from 'mongoose'
+const morgan = require('morgan')
+
+require('dotenv').config()
+
 const app = express()
 
-app.get('/api/:message', (req, res) => {
-  res.status(200).send(req.params.message)
-})
+// DB connection
 
-app.listen(8000, () => console.log(`Server is running on port 8000`))
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB connected'))
+  .catch(err => console.log('DB error', err))
+// // middlewares
+app.use(cors())
+app.use(morgan('dev'))
+
+// route middleware
+app.use('/api', router)
+// readdirSync('./routes').map(r => app.use('/api', require(`./routes/${r}`)))
+
+const port = process.env.PORT || 8000
+app.listen(port, () => console.log(`Server is running on port ${port}`))
