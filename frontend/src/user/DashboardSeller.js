d5beeba25a86c58ee3320c473a1f5d120327b01e
Jake Lira <llira09@gmail.com>
2021-03-05 12:01:04 -0600

    updated seller dashboard, adding stripe

--- frontend/src/user/DashboardSeller.js
+++ frontend/src/user/DashboardSeller.js
@@ -1,8 +1,45 @@
 import DashboardNav from '../components/DashboardNav'
 import ConnectNav from '../components/ConnectNav'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { HomeOutlined } from '@ant-design/icons'
 
 const DashboardSeller = () => {
+  const { auth } = useSelector(state => ({ ...state }))
+  const connected = () => (
+    <div className='container-fluid pt-3'>
+      <div className='row'>
+        <div className='col-md-10'>
+          <h2>Your Rentals Homes</h2>
+        </div>
+        <div className='col-md-2'>
+          <Link to='/homes/new' className='btn btn-primary'>
+            + Add New
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+
+  const notConnected = () => (
+    <div className='container-fluid pt-3'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3 text-center'>
+          <div className='p-5-pointer'>
+            <HomeOutlined className='h2' />
+            <h4>Setup stripe for rentals</h4>
+            <p>Transfer earnings to your bank account</p>
+            <button className='btn btn-primary'>Setup Payouts</button>
+            <p className='text-muted'>
+              <small>
+                You will be redirected to stripe for onboarding process.
+              </small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
   return (
     <>
       <div className='container-fluid bg-secondary p-5'>
@@ -11,18 +48,12 @@ const DashboardSeller = () => {
       <div className='container-fluid'>
         <DashboardNav />
       </div>
-      <div className='container-fluid pt-3'>
-        <div className='row'>
-          <div className='col-md-10'>
-            <h2>Your Rentals Homes</h2>
-          </div>
-          <div className='col-md-2'>
-            <Link to='/homes/new' className='btn btn-primary'>
-              + Add New
-            </Link>
-          </div>
-        </div>
-      </div>
+      {auth &&
+      auth.user &&
+      auth.user.stripe_seller &&
+      auth.user.stripe_seller.charges_enabled
+        ? connected()
+        : notConnected()}
     </>
   )
 }
